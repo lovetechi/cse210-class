25080dc1a6c5825742f1542bc3da82949ad58545
danchinam <constructionam778@gmail.com>
2025-10-29 15:24:41 +0100

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("what is your first name? .");
+        Console.WriteLine("what is your first name?.");
         string firstName = Console.ReadLine();
         
-        Console.WriteLine("what is your last name? .");
+        Console.WriteLine("what is your last name?.");
         string lastName = Console.ReadLine();
 
         Console.WriteLine($"your name is {lastName} {firstName} {lastName}.");
