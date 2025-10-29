574238ec21afa1b7b3acb6efdb37c503cc344896
danchinam <constructionam778@gmail.com>
2025-10-29 14:52:57 +0100

    Update Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+           Random randomGererator = new Random();
+           int magicNumber = randomGererator.Next(1, 101);
+
+           int guess = -1;
+           while (guess != magicNumber)
+           {
+                Console.WriteLine("what is your guess?.");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                Console.WriteLine("higher");
+                }
+                 else if (magicNumber < guess)
+                {
+                Console.WriteLine("lower");
+              }
+
+                else
+             {
+                Console.WriteLine("you guessed it!");
+             }
+         }
     }
 }
