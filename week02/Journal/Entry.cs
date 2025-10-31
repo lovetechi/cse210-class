7c5c499b8cba467246eb713a89163effd123480c
danchinam <constructionam778@gmail.com>
2025-10-31 17:11:23 +0100

    Create Entry.cs

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Entry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public string Date { get; set; }
+
+    public Entry(string prompt, string response, string date)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+    }
+
+    public override string ToString()
+    {
+        return $"{Date} - {Prompt}\n{Response}";
+    }
+}
