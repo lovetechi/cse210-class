ad9a2340c27dfd73474c7d3c9b7c41344f51320a
danchinam <constructionam778@gmail.com>
2025-12-09 20:08:51 +0100

    Update Program.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        var activities = new List<Activity>();
+
+        activities.Add(new RunningActivity(new DateTime(2022,11,3), 30, 3.0));
+        activities.Add(new CyclingActivity(new DateTime(2022,11,3), 30, 12.0));
+        activities.Add(new SwimmingActivity(new DateTime(2022,11,3), 30, 60)); // 60 laps
+
+        foreach (var a in activities)
+        {
+            Console.WriteLine(a.GetSummary());
+        }
     }
 }
